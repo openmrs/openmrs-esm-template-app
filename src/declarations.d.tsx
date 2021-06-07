baa178303125d48d30b8a3498987d406644a9a61
Brandon Istenes <bistenes@gmail.com>
2021-06-07 13:46:55 -0700

    Simplify declarations

--- src/declarations.d.tsx
+++ src/declarations.d.tsx
@@ -1,6 +1,1 @@
-// This entire file will be deleted once we have typescript definitions
-// for @openmrs/esm-framework published to npm
-declare type Observable<T> = import('rxjs').Observable<T>;
-
 declare module '*.css';
-declare module '*.scss';
