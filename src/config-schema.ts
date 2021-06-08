b5cf45074a7e2bc271d097dd6897ea3f8680073a
Brandon Istenes <bistenes@gmail.com>
2021-06-07 20:36:26 -0700

    Document config-schema

--- src/config-schema.ts
+++ src/config-schema.ts
@@ -1,5 +1,25 @@
 import { Type, validator } from '@openmrs/esm-framework';
 
+/**
+ * This is the config schema. It expects a configuration object which
+ * looks like this:
+ * 
+ * ```json
+ * { "casualGreeting": true, "whoToGreet": ["Mom"] }
+ * ```
+ * 
+ * In OpenMRS Microfrontends, all config parameters are optional. Thus,
+ * all elements must have a reasonable default. A good default is one
+ * that works well with the reference application.
+ * 
+ * To understand the schema below, please read the configuration system
+ * documentation:
+ *   https://openmrs.github.io/openmrs-esm-core/#/main/config
+ * Note especially the section "How do I make my module configurable?"
+ *   https://openmrs.github.io/openmrs-esm-core/#/main/config?id=im-developing-an-esm-module-how-do-i-make-it-configurable
+ * and the Schema Reference
+ *   https://openmrs.github.io/openmrs-esm-core/#/main/config?id=schema-reference
+ */
 export const configSchema = {
   casualGreeting: {
     _type: Type.Boolean,
