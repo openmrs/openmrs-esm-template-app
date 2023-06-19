12c1fccaee256a828fb135953044faf5ac6f7604
Dennis Kigen <kigen.work@gmail.com>
2023-06-20 01:58:10 +0300

    (BREAKING) Migrate `esm-template-app` to use routes.json

--- src/index.ts
+++ src/index.ts
@@ -4,26 +4,22 @@
  * connects the app shell to the React application(s) that make up this
  * microfrontend.
  */
-
 import { getAsyncLifecycle, defineConfigSchema } from "@openmrs/esm-framework";
 import { configSchema } from "./config-schema";
 
-/**
- * This tells the app shell the version of this app. We inject this variable
- * via the Webpack DefinePlugin, so first, in order to keep TypeScript happy,
- * we need to tell it that this is an available variable in this scope. At
- * build-time the __VERSION__ variable is replaced with the version from
- * package.json
- */
-declare var __VERSION__: string;
-const version = __VERSION__;
+const moduleName = "@openmrs/esm-template-app";
+
+const options = {
+  featureName: "hello-world",
+  moduleName,
+};
 
 /**
  * This tells the app shell how to obtain translation files: that they
  * are JSON files in the directory `../translations` (which you should
  * see in the directory structure).
  */
-const importTranslation = require.context(
+export const importTranslation = require.context(
   "../translations",
   false,
   /.json$/,
@@ -31,73 +27,31 @@ const importTranslation = require.context(
 );
 
 /**
- * This tells the app shell what versions of what OpenMRS backend modules
- * are expected. Warnings will appear if suitable modules are not
- * installed. The keys are the part of the module name after
- * `openmrs-module-`; e.g., `openmrs-module-fhir2` becomes `fhir2`.
- */
-const backendDependencies = {
-  fhir2: "^1.2.0",
-  "webservices.rest": "^2.2.0",
-};
-
-/**
  * This function performs any setup that should happen at microfrontend
  * load-time (such as defining the config schema) and then returns an
  * object which describes how the React application(s) should be
  * rendered.
- *
- * In this example, our return object contains a single page definition.
- * It tells the app shell that the default export of `hello.tsx`
+ */
+export function startupApp() {
+  defineConfigSchema(moduleName, configSchema);
+}
+
+/**
+ * This named export tells the app shell that the default export of `hello.tsx`
  * should be rendered when the route matches `hello`. The full route
  * will be `openmrsSpaBase() + 'hello'`, which is usually
  * `/openmrs/spa/hello`.
  */
-function setupOpenMRS() {
-  const moduleName = "@openmrs/esm-template-app";
+export const hello = () => getAsyncLifecycle(() => import("./hello"), options);
 
-  const options = {
-    featureName: "hello-world",
-    moduleName,
-  };
+/**
+ * The following are named exports for the extensions defined in this frontend modules. See the `routes.json` file to see how these are used.
+ */
+export const redBox = () =>
+  getAsyncLifecycle(() => import("./boxes/extensions/red-box"), options);
 
-  defineConfigSchema(moduleName, configSchema);
+export const blueBox = () =>
+  getAsyncLifecycle(() => import("./boxes/extensions/blue-box"), options);
 
-  return {
-    pages: [
-      {
-        load: getAsyncLifecycle(() => import("./hello"), options),
-        route: "hello",
-      },
-    ],
-    extensions: [
-      {
-        name: "Red box",
-        load: getAsyncLifecycle(
-          () => import("./boxes/extensions/red-box"),
-          options
-        ),
-        slot: "Boxes",
-      },
-      {
-        name: "Blue box",
-        load: getAsyncLifecycle(
-          () => import("./boxes/extensions/blue-box"),
-          options
-        ),
-        slot: "Boxes",
-        // same as `slots: ["Boxes"],`
-      },
-      {
-        name: "Brand box",
-        load: getAsyncLifecycle(
-          () => import("./boxes/extensions/brand-box"),
-          options
-        ),
-        slot: "Boxes",
-      },
-    ],
-  };
-}
-
-export { backendDependencies, importTranslation, setupOpenMRS, version };
+export const brandBox = () =>
+  getAsyncLifecycle(() => import("./boxes/extensions/brand-box"), options);
