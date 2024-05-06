804873eacadfe8d37041f5bcacc62234f438d5e3
Nethmi Rodrigo <neth23rodrigo@gmail.com>
2024-05-06 18:41:49 +0530

    (chore) Bump openmrs framework dependancies (#28)
    * (chore) Bump openmrs framework dependancies
    
    * (fix): add esm-util to moduleNameWrapper

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,16 @@
 /**
  * @returns {Promise<import('jest').Config>}
  */
-const path = require('path');
+const path = require("path");
 
 module.exports = {
   collectCoverageFrom: [
-    '**/src/**/*.component.tsx',
-    '!**/node_modules/**',
-    '!**/vendor/**',
-    '!**/src/**/*.test.*',
-    '!**/src/declarations.d.ts',
-    '!**/e2e/**',
+    "**/src/**/*.component.tsx",
+    "!**/node_modules/**",
+    "!**/vendor/**",
+    "!**/src/**/*.test.*",
+    "!**/src/declarations.d.ts",
+    "!**/e2e/**",
   ],
   transform: {
     "^.+\\.tsx?$": ["@swc/jest"],
@@ -18,12 +18,13 @@ module.exports = {
   transformIgnorePatterns: ["/node_modules/(?!@openmrs)"],
   moduleNameMapper: {
     "@openmrs/esm-framework": "@openmrs/esm-framework/mock",
+    "@openmrs/esm-utils": "@openmrs/esm-framework/mock",
     "\\.(s?css)$": "identity-obj-proxy",
     "^lodash-es/(.*)$": "lodash/$1",
     "^dexie$": require.resolve("dexie"),
   },
   setupFilesAfterEnv: ["<rootDir>/src/setup-tests.ts"],
-  testPathIgnorePatterns: [path.resolve(__dirname, 'e2e')],
+  testPathIgnorePatterns: [path.resolve(__dirname, "e2e")],
   testEnvironment: "jsdom",
   testEnvironmentOptions: {
     url: "http://localhost/",
