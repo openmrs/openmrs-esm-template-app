67816fdfd0afb4e56ff33b043949893666d5954d
Usama Idriss Kakumba <usamakakumba@gmail.com>
2024-10-03 10:10:01 +0300

    (chore) cleanup initial setup

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -2,5 +2,3 @@ declare module '@carbon/react';
 declare module '*.css';
 declare module '*.scss';
 declare module '*.png';
-
-declare type SideNavProps = object;
--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,18 @@
-/**
- * This is the entrypoint file of the application. It communicates the
- * important features of this microfrontend to the app shell. It
- * connects the app shell to the React application(s) that make up this
- * microfrontend.
- */
-import { getAsyncLifecycle, defineConfigSchema } from '@openmrs/esm-framework';
+import { defineConfigSchema, getSyncLifecycle } from '@openmrs/esm-framework';
 import { configSchema } from './config-schema';
+import patientBillingStatusSummary from './components/billing-status-summary.component';
 
-const moduleName = '@openmrs/esm-template-app';
+const moduleName = '@openmrs/esm-patient-billing-status-app';
 
 const options = {
   featureName: 'root-world',
   moduleName,
 };
 
-/**
- * This tells the app shell how to obtain translation files: that they
- * are JSON files in the directory `../translations` (which you should
- * see in the directory structure).
- */
 export const importTranslation = require.context('../translations', false, /.json$/, 'lazy');
 
-/**
- * This function performs any setup that should happen at microfrontend
- * load-time (such as defining the config schema) and then returns an
- * object which describes how the React application(s) should be
- * rendered.
- */
 export function startupApp() {
   defineConfigSchema(moduleName, configSchema);
 }
 
-/**
- * This named export tells the app shell that the default export of `root.component.tsx`
- * should be rendered when the route matches `root`. The full route
- * will be `openmrsSpaBase() + 'root'`, which is usually
- * `/openmrs/spa/root`.
- */
-export const root = getAsyncLifecycle(() => import('./root.component'), options);
-
-/**
- * The following are named exports for the extensions defined in this frontend modules. See the `routes.json` file to see how these are used.
- */
-export const redBox = getAsyncLifecycle(() => import('./boxes/extensions/red-box.component'), options);
-
-export const blueBox = getAsyncLifecycle(() => import('./boxes/extensions/blue-box.component'), options);
-
-export const brandBox = getAsyncLifecycle(() => import('./boxes/extensions/brand-box.component'), options);
+export const patientBillingStatusOverview = getSyncLifecycle(patientBillingStatusSummary, options);
